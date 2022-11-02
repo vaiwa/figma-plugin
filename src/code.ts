a399023467e33abc1d9c1ad2a158541a4d2ea04a
Jan Návrat <jan.navrat@gmail.com>
2022-11-02 15:36:18 +0100

    copy OpenDesignFigmaSource

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,6 @@
 // import Clipboard from 'clipboard'
 import _ from 'lodash' // TODO Remove lodash
+import { version } from '../package.json'
 
 import { dispatch, handleEvent } from './codeMessageHandler'
 
@@ -7,7 +8,31 @@ figma.showUI(__html__, { height: 240, width: 300 })
 
 console.log('THIS', this)
 
-const sendSelectionchange = () => dispatch('selectionchange', figma.currentPage.selection)
+const getSource = () => {
+  const content = figma.currentPage.selection.map((node) => nodeToObject(node))
+
+  if (!content.length) return null
+
+  return { type: 'OPEN_DESIGN_FIGMA_PLUGIN_SOURCE', version, content }
+}
+
+const nodeToObject = (node) => {
+  const props = Object.entries(Object.getOwnPropertyDescriptors(node.__proto__))
+  const blacklist = ['parent', 'children', 'removed']
+  const obj: any = { id: node.id, type: node.type }
+  if (node.parent) obj.parent = { id: node.parent.id, type: node.type }
+  for (const [name, prop] of props) {
+    if (prop.get && blacklist.indexOf(name) < 0) {
+      obj[name] = prop.get.call(node)
+      if (typeof obj[name] === 'symbol') obj[name] = 'Mixed'
+    }
+  }
+  if (node.children) obj.children = node.children.map((child) => nodeToObject(child))
+  if (node.masterComponent) obj.masterComponent = nodeToObject(node.masterComponent)
+  return obj
+}
+
+const sendSelectionchange = () => dispatch('selectionchange', getSource())
 figma.on('selectionchange', () => sendSelectionchange())
 sendSelectionchange() // initial send
 
